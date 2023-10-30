d525ca9c73653ec8f56cc5683037ae2048b18729
Daniel Westerlund <westerlund.da@gmail.com>
2023-10-30 20:14:51 +0100

    Initialize Firebase app with Firebase Admin SDK

--- src/main/java/com/frontendmentor/todoapp/TodoappApplication.java
+++ src/main/java/com/frontendmentor/todoapp/TodoappApplication.java
@@ -1,13 +1,17 @@
 package com.frontendmentor.todoapp;
 
+import com.google.firebase.FirebaseApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappApplication.class, args);
+
+        FirebaseApp.initializeApp();
+
+    }
 
 }
